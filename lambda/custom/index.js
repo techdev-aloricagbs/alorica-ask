abd120b2208c084c51f68a9c5bd227ff47a5ddb9
John Michael Ferraris <johnmichaelferraris@gmail.com>
2017-11-09 20:46:59 +0800

    Prototype handling of slots
    Handles cybergate, fort bonifacio, egs and alo

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -2,15 +2,30 @@
 
 var Alexa = require("alexa-sdk");
 
+const data = {
+  "employees": {
+    "count": {
+      "alo": 429,
+      "egs": 550,
+      "cyber gate": 16,
+      "fort bonifacio": 61
+    }
+  }
+};
 var handlers = {
   "HelloIntent": function () {
-    this.response.speak("Hello, Alorica"); 
+    this.response.speak("Hello, Alorica");
     this.emit(':responseReady');
   },
   "LaunchRequest": function () {
-    this.response.speak("Welcome to Alorica"); 
+    this.response.speak("Welcome to Alorica");
     this.emit(':responseReady');
-  }
+  },
+  "CountIntent": function() {
+    let siteName = this.event.request.intent.slots.siteName.value.toLowerCase();
+    this.response.speak(sitename + " site has " + data.employees.count[siteName] + " employees.")
+    this.emit(':responseReady');
+  },
 };
 
 exports.handler = function(event, context, callback){
