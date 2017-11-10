4a6cdfb95ac9108d81ceb69a1a9409e054ab305c
John Michael Ferraris <johnmichaelferraris@gmail.com>
2017-11-10 22:19:05 +0800

    Added DBIntent for db queries

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -2,6 +2,19 @@
 
 var Alexa = require("alexa-sdk");
 
+var pg = require("pg");
+var client = new pg.Client({
+  host: 'your-host',
+  user: 'your-user',
+  password: 'your-password',
+  database: 'your-db-name',
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+  port: 5432
+})
+client.connect();
+
 const data = {
   "seat": {
     "count": {
@@ -41,6 +54,29 @@ var handlers = {
   "LaunchRequest": function () {
     this.response.speak("Welcome to Alorica");
     this.emit(':responseReady');
+  },
+  "DBIntent": function () {
+    let field = this.event.request.intent.slots.field.value;
+    let fieldValue = this.event.request.intent.slots.fieldValue.value.toLowerCase();
+    let query = `SELECT ${field} from apps_dev where ${field} = '${fieldValue}'`;
+    console.log(query);
+    client.query(query)
+            .then(function (result) {
+              console.log(result);
+              if (result.rowCount == 0) {
+                this.response.speak(this.event.request.intent.slots.employeeName.value + ' is not around.');
+                this.emit(':responseReady');
+              } else {
+                this.response.speak(result.rows[0].name + ' is around.');
+                this.emit(':responseReady');
+              }
+              this.context.done(null);
+            }.bind(this))
+            .catch(function(e) {
+              this.response.speak('I cannot access that specific employee information');
+              this.emit(':responseReady');
+              this.context.done(null);
+            }.bind(this))
   },
   "CountIntent": function() {
     let siteName = this.event.request.intent.slots.siteName.value.toLowerCase();
