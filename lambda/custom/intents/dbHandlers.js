db17f36b0400e83fa0d24ec7795af879e5f8ad28
John Michael Ferraris <johnmichaelferraris@gmail.com>
2017-11-17 22:37:50 +0800

    Utilized `asyncawait` to shorten up promises/callbacks

--- lambda/custom/intents/dbHandlers.js
+++ lambda/custom/intents/dbHandlers.js
@@ -1,31 +1,30 @@
 
 const db = require('../modules/db');
+const async = require('asyncawait/async');
+const await = require('asyncawait/await');
+
 let dbHandlers = {
-  "DBIntent": function () {
+  "DBIntent": async(function () {
     let field = this.event.request.intent.slots.field.value;
     let fieldValue = this.event.request.intent.slots.fieldValue.value.toLowerCase();
     let query = `SELECT ${field} from apps_dev where ${field} = '${fieldValue}'`;
     console.log('Querying database..');
-    db.any(query)
-            .then(function (data) {
-              console.log('Checking results...');
-              if (data.length == 0) {
-                this.response.speak(this.event.request.intent.slots.employeeName.value + ' is not around.');
-                this.emit(':responseReady');
-              } else {
-                this.response.speak(data[0].name + ' is around.');
-                this.emit(':responseReady');
-              }
-              console.log('Done!')
-              this.context.done(null);
-            }.bind(this))
-            .catch(function(e) {
-              console.error(e.stack);
-              this.response.speak('I cannot access that specific employee information');
-              this.emit(':responseReady');
-              this.context.done(null);
-            }.bind(this))
-  }
+    try {
+      let data = await(db.any(query));
+      if (data.length === 0) {
+        this.response.speak(this.event.request.intent.slots.employeeName.value + ' is not around.');
+        this.emit(':responseReady');
+      } else {
+        this.response.speak(data[0].name + ' is around.');
+        this.emit(':responseReady');
+      }
+      console.log('Done!');
+    } catch (error) {
+      console.error(error.stack);
+      this.response.speak('I cannot access that specific employee information');
+      this.emit(':responseReady');
+    }
+  }),
 };
 
 module.exports = dbHandlers;
