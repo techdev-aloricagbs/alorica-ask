43087cf90a089c9b963c58b3b45dd073ab4cfd8f
John Michael Ferraris <johnmichaelferraris@gmail.com>
2017-11-17 16:29:37 +0800

    Added informative logs

--- lambda/custom/intents/dbHandlers.js
+++ lambda/custom/intents/dbHandlers.js
@@ -16,10 +16,10 @@ let dbHandlers = {
     let field = this.event.request.intent.slots.field.value;
     let fieldValue = this.event.request.intent.slots.fieldValue.value.toLowerCase();
     let query = `SELECT ${field} from apps_dev where ${field} = '${fieldValue}'`;
-    console.log(query);
+    console.log('Querying database..');
     client.query(query)
             .then(function (result) {
-              console.log(result);
+              console.log('Checking results...');
               if (result.rowCount == 0) {
                 this.response.speak(this.event.request.intent.slots.employeeName.value + ' is not around.');
                 this.emit(':responseReady');
@@ -27,9 +27,11 @@ let dbHandlers = {
                 this.response.speak(result.rows[0].name + ' is around.');
                 this.emit(':responseReady');
               }
+              console.log('Done!')
               this.context.done(null);
             }.bind(this))
             .catch(function(e) {
+              console.error(e.stack);
               this.response.speak('I cannot access that specific employee information');
               this.emit(':responseReady');
               this.context.done(null);
